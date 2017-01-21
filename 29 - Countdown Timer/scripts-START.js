001696f3ccc594174541c33cae178d0509c2be8f
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-21 09:33:12 -0600

    add funcs for displaying time left and end time.

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,9 +1,15 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+	// Clear any existing timers
+	clearInterval(countdown);
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	displayTimeLeft(seconds);
+	displayEndTime(then);
 
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -16,5 +22,26 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
+	const minutes = Math.floor(seconds / 60);
+	const remainderSeconds = seconds % 60;
+	const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+	document.title = display;
+	timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+	const end = new Date(timestamp);
+	const hour = end.getHours();
+	const adjustedHour = hour > 12 ? hour - 12 : hour;
+	const minutes = end.getMinutes();
+	const adjustedMinutes = minutes < 10 ? '0' : '';
+	endTime.textContent = `The world ends at ${adjustedHour}:${adjustedMinutes}${minutes}`;
+}
+
+function startTimer() {
+	const seconds = parseInt(this.dataset.time);
+	timer(seconds);
 	console.log(seconds);
 }
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
